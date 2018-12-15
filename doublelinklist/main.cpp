7c1de7e943c1cfb1f17ec2c516921144c0817acf
Bishal Blue <blue.bishal@rocketmail.com>
2018-12-15 10:48:20 +0530

    Initial Commit

---
+++ doublelinklist/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  doublelinklist
+//
+//  Created by Bishal Blue on 15/12/18.
+//  Copyright © 2018 Bishal Blue. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
